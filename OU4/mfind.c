8286112913883d49533ef7ffc85086138e7963e9
markon93 <makkin.n@hotmail.com>
2017-10-18 11:43:49 +0200

    almost done...

--- OU4/mfind.c
+++ OU4/mfind.c
@@ -11,12 +11,16 @@
 #include "checkComLine.h"
 
 pthread_mutex_t m;
+pthread_mutex_t m2;
+pthread_cond_t cond;
 
-/* Checks if a given path is a regular file or no. */
+int waitingThreads = 0;
+
+/* Checks if a given path is a regular file or not. */
 int isRegularFile(const char *path){
-    struct stat path_stat;
-    lstat(path, &path_stat);
-    return S_ISDIR(path_stat.st_mode);
+  struct stat path_stat;
+  lstat(path, &path_stat);
+  return S_ISDIR(path_stat.st_mode);
 }
 
 void* traverse(void* com){
@@ -26,83 +30,86 @@ void* traverse(void* com){
 	while(true){
 		pthread_mutex_lock(&m);
 
-		// Pick a directory from the directory queue
-		char* dir;
-		if(!q_isEmpty(c -> dirQueue)){
-			dir = q_peek(c -> dirQueue);
-		}
-		 else if(q_isEmpty(c -> dirQueue)){
-     	 	printf("Empty\n");
-      		pthread_mutex_unlock(&m);
-     	 	break;
-    	}
-		q_dequeue(c -> dirQueue);
-
-		// If the queue is not empty, let the other threads continue, else the
-		// first thread must look for subdirectories before continuing.
-		bool isUnlocked = false;
-		if(!q_isEmpty(c -> dirQueue)){
-			isUnlocked = true;
-			pthread_mutex_unlock(&m);
-		}
-
-		// Read the files in the directory
-		// If the file is a directory, put it in the queue
-		struct dirent *pDirent;
-		DIR* d = opendir(dir);
-		numDirs++;
-		if(d == NULL){
-			printf(":::%s:::\n", dir);
-			perror("");
-			exit(5);
-		}
-
-    char* dirWithSlash = (char*)malloc(strlen(dir)*sizeof(dir) + 1);
-    strcpy(dirWithSlash, dir);
-
-    dirWithSlash = strcat(dirWithSlash,"/");
-
-		while((pDirent = readdir(d)) != NULL){
-			if(strcmp(pDirent -> d_name, ".") &&
-      strcmp(pDirent -> d_name, "..")){
-
-        char* fullPath = (char*)malloc(strlen(dirWithSlash)*sizeof(dirWithSlash)+
-      strlen(pDirent -> d_name)*sizeof(pDirent -> d_name));
-        strcpy(fullPath, dirWithSlash);
-        fullPath = strcat(fullPath, pDirent -> d_name);
-
-        char* fullPathCopy = malloc(strlen(fullPath)*sizeof(fullPath));
-        strcpy(fullPathCopy, fullPath);
-
-		struct stat path_stat;
-    	lstat(fullPath, &path_stat);
-
-		// Add new directories to queue if there are any
-		if(S_ISDIR(path_stat.st_mode) &&
-			 (access(fullPath, R_OK) == 0)){
-			q_enqueue(c -> dirQueue, fullPathCopy);
- 	   	}
-
-		// If the filename is in the current directory, print out the path.
-		if(!strcmp(c -> name, pDirent -> d_name)){
-				if((S_ISREG(path_stat.st_mode) && (c -> type == 'f')) ||
-					(S_ISDIR(path_stat.st_mode) && (c -> type == 'd'))||
-					(S_ISLNK(path_stat.st_mode) && (c -> type == 'l'))){
-					printf("\n%s\n",fullPathCopy);
-				}
-		}
-        free(fullPath);
+    if(q_isEmpty(c -> dirQueue)){
+      waitingThreads++;
+      pthread_mutex_unlock(&m);
+      if(waitingThreads != c -> nrthr - 1){
+        pthread_cond_broadcast(&cond);
       }
-		}
-		closedir(d);
-    free(dirWithSlash);
 
-    if(!isUnlocked){
-		pthread_mutex_unlock(&m);
-	}
+      else{
+        printf("Thread %lu waiting...\n",pthread_self());
+        pthread_cond_wait(&cond, &m2);
+      }
 
+      if(waitingThreads == c -> nrthr - 1){
+        break;
+      }
+      else{
+        waitingThreads--;
+        continue;
+      }
+    }
+    else{
+    		// Pick a directory from the directory queue
+        char* dir = q_peek(c -> dirQueue);
+        q_dequeue(c -> dirQueue);
+        pthread_mutex_unlock(&m);
+        // Read the files in the directory
+    		// If the file is a directory, put it in the queue
+    		struct dirent *pDirent;
+    		DIR* d = opendir(dir);
+    		numDirs++;
+    		if(d == NULL){
+    			perror("");
+    			exit(5);
+    		}
+
+        char* dirWithSlash = (char*)malloc(strlen(dir)*sizeof(dir) + 1);
+        strcpy(dirWithSlash, dir);
+
+        dirWithSlash = strcat(dirWithSlash,"/");
+
+    		while((pDirent = readdir(d)) != NULL){
+    			if(strcmp(pDirent -> d_name, ".") &&
+          strcmp(pDirent -> d_name, "..")){
+
+            char* fullPath = (char*)malloc(strlen(dirWithSlash)*sizeof(dirWithSlash)+
+          strlen(pDirent -> d_name)*sizeof(pDirent -> d_name));
+            strcpy(fullPath, dirWithSlash);
+            fullPath = strcat(fullPath, pDirent -> d_name);
+
+            char* fullPathCopy = malloc(strlen(fullPath)*sizeof(fullPath));
+            strcpy(fullPathCopy, fullPath);
+
+    		struct stat path_stat;
+        	lstat(fullPath, &path_stat);
+
+    		// Add new directories to queue if there are any
+    		if(S_ISDIR(path_stat.st_mode) &&
+    			 (access(fullPath, R_OK) == 0)){
+    			q_enqueue(c -> dirQueue, fullPathCopy);
+          pthread_cond_broadcast(&cond);
+     	  }
+
+    		// If the filename is in the current directory, print out the path.
+    		if(!strcmp(c -> name, pDirent -> d_name)){
+    				if((S_ISREG(path_stat.st_mode) && (c -> type == 'f')) ||
+    					(S_ISDIR(path_stat.st_mode) && (c -> type == 'd'))||
+    					(S_ISLNK(path_stat.st_mode) && (c -> type == 'l'))){
+    					printf("\n%s\n",fullPathCopy);
+    				}
+    		}
+            free(fullPath);
+          }
+    		}
+    		closedir(d);
+        free(dirWithSlash);
+    }
   }
 	printf("Thread: %lu Reads: %d\n", pthread_self(), numDirs);
+  //sleep(2); // Should be enough to make the last few threads wait
+  pthread_cond_broadcast(&cond);
   return NULL;
 }
 
