149b28b05bf0c52d966a1c500b421c4f210095c0
Marko <makkin.n@hotmail.com>
2017-10-19 11:10:03 +0200

    sigh again

--- OU4/queue.c
+++ OU4/queue.c
@@ -17,9 +17,9 @@ void q_dequeue(queue* q){
 void q_enqueue(queue* q, void* element){
 	node* n = list_first(q);
 
-	while(!list_end(q,n)){
-		n = list_next(q,n);
-	}
+	//while(!list_end(q,n)){
+	//	n = list_next(q,n);
+	//}
 	list_insert(q, n, element);
 }
 
